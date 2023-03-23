0d6b16e3ba205145b1be1507cd18d575cb2d13fe
Jinseon Lee <jineelee11@gmail.con>
2023-03-23 16:59:26 +0900

    [Feat]프로젝트 등록 DB저장(이미지,펀딩기간제외)

--- src/main/java/com/project/repository/ItemRepository.java
+++ src/main/java/com/project/repository/ItemRepository.java
@@ -1,9 +1,20 @@
+
 package com.project.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
 import com.project.entity.Item;
 
-public interface ItemRepository extends JpaRepository<Item, Long> {
 
+
+public interface ItemRepository extends JpaRepository<Item, Long>{
+
+	//List<Item> findByItemNm(String itemNm);
+	
+	//List<Item> findByPriceLessThan(Integer Price);
+	
+	//List<Item> findByPriceLessThanOrderByPriceDesc(Integer price);
 }
