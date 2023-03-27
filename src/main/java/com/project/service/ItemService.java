1494ca772caf4386aa26446aea5b1fcb8bf20cc5
yerim park <qdf22333@hanmail.net>
2023-03-27 19:37:35 +0900

    [DOCS] 헤더추가, gradle 업그레이드

--- src/main/java/com/project/service/ItemService.java
+++ src/main/java/com/project/service/ItemService.java
@@ -1,5 +1,7 @@
 package com.project.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -35,7 +37,23 @@ public class ItemService {
 		
 		return item;
 	}
-	
+
+	//idx 값으로 상세페이지 아이템 불러오기
+		public List<Item> getList() {
+			return this.itemRepository.findAll();
+		}
+		/*
+		public Item getItem(Long idx) {
+			Optional<Item> item = this.itemRepository.findByIdx(idx);
+			if (item.isPresent()) {
+				return item.get();
+				
+			}else {
+				 throw new DataNotFoundException("item not found");
+			}
+		}
+		 */
+		
 	
 	
 
